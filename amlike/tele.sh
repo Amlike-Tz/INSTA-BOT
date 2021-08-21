e32900bd8ff892db687d65d3695cec5488e2375b
Amlike-Tz <83519622+Amlike-Tz@users.noreply.github.com>
2021-08-21 17:18:30 +0300

    Add files via upload

---
+++ amlike/tele.sh
@@ -0,0 +1,20 @@
+#! usr/bin/bash
+
+echo ""
+
+sleep 2
+
+am start -a android.intent.action.VIEW -d https://t.me/TeamAmlikeTz
+
+echo " "
+sleep 3
+echo "WElcome Back...........\n\tSasa Hiv unalud katika Main Menu Ya hii Tool"
+echo " "
+sleep 3
+
+echo "Thankyou Kwa Kuwasiliana Na Developer!!🤝🤝🤞"
+sleep 6
+clear
+
+cd $CHOME
+cd INSTA-BOT && python start.py
---
+++ install-requirement.sh
@@ -0,0 +1,71 @@
+#! usr/bin/bash
+
+clear
+
+center() {
+  termwidth=$(stty size | cut -d" " -f2)
+  padding="$(printf '%0.1s' ={1..500})"
+  printf '%*.*s %s %*.*s\n' 0 "$(((termwidth-2-${#1})/2))" "$padding" "$1" 0 "$(((termwidth-1-${#1})/2))" "$padding"
+}
+
+echo -e "\033[92m"
+echo
+echo
+
+center "STARING TO INSTALL DEPENDENCIES"
+sleep 3
+
+cod="\033[0m"
+o="\033[91m"
+grn="\033[92m"
+blu="\033[34m"
+
+cd $SCHOME
+
+#Pillow Installatation
+
+
+python3 -m pip install --upgrade pip
+
+pip install wheel
+
+pkg install libjpeg-turbo LDFLAGS="-L/system/lib/" CFLAGS="-I/data/data/com.termux/files/usr/include/"
+
+clear
+
+center "STARING TO INSTALL PILLOW"
+
+sleep 2
+
+pip install Pillow
+
+clear
+
+echo
+echo
+echo
+center "STARING TO INSTALL INSTA-BOT BY AMKIKETZ"
+
+sleep 3
+
+pip install instabot
+ 
+
+
+clear
+
+sleep 1
+
+echo
+echo
+
+echo -e "$blue HONGERA INSTALLATION COMPLITED"
+
+sleep 2
+
+cd
+
+cd INSTA-BOT
+
+python start.py
+
